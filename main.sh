f41ff101270533b2446263c3ccf1ac5333f013cd
RetributionByRevenue <56832202+RetributionByRevenue@users.noreply.github.com>
2025-10-08 18:22:46 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -12,9 +12,6 @@ echo '    key_mgmt=WPA-PSK' >> /tmp/wpa_supplicant_ap.conf
 echo '    psk="12345678"' >> /tmp/wpa_supplicant_ap.conf
 echo '}' >> /tmp/wpa_supplicant_ap.conf
 
-#ip -6 addr flush wlan0
-echo "nameserver 137.220.33.124" > /etc/resolv.conf
-
 
 #showcase instance of wpa_supplicant is running
 ps aux | grep "wpa_supp"
@@ -40,20 +37,9 @@ dbus-send --system --print-reply --dest=net.connman /net/connman/technology/wifi
 
 read -t 100 -n 1 -p "Please ensure your VPN connection is active and applied to the default route (waiting for 100s, press any key to continue)..." key
 
-# Store interfaces in a variable
-INTERFACES=$(ifconfig | grep "Link encap" | awk '{print $1}' | grep -vE "^(lo|rmnet_ipa0|rndis0|wlan1)$")
-
-# Print the interfaces
-echo "Matching Interfaces:"
-for iface in $INTERFACES; do
-    echo "$iface"
-done
-
-# Loop through interfaces and apply MASQUERADE
-echo "Applying MASQUERADE rules:"
-for iface in $INTERFACES; do
-    echo "Adding NAT rule for interface: $iface"
-    iptables -t nat -A POSTROUTING -s 10.10.0.0/16 -o "$iface" -j MASQUERADE
+for iface in $(ifconfig | grep "Link encap" | awk '{print $1}' | grep -vE "^(lo|rmnet_ipa0|rndis0|wlan1)$"); do
+  echo "$iface"
+  iptables -t nat -A POSTROUTING -s 10.10.0.0/16 -o "$iface" -j MASQUERADE
 done
 
 echo "now we see test_ap is online"
@@ -63,3 +49,4 @@ netstat -tunlp
 pkill udhcpd
 
 /home/defaultuser/python/venv/bin/python    /home/defaultuser/python/wlan1_dhcp_server.py
+
